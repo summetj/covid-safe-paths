07fa48bcf3e6cd211a349c6c7b10f83bad59e6bf
John Schoeman <johnschoeman1617@gmail.com>
2020-06-02 14:39:03 -0400

    setup some api boiler plate

--- app/api/ExposureNotificationAPI.js
+++ app/api/ExposureNotificationAPI.js
@@ -1,11 +1,65 @@
-const postDiagnosisKeys = () => {};
+const exposureNotificationServerUrl = 'https://example.com';
 
-const getDiagnosisKeyFileURLs = () => {};
+const defaultHeaders = {
+  'content-type': 'application/json',
+  accept: 'application/json',
+};
 
-const downloadDiagnosisKeyFile = () => {};
+export const postDiagnosisKeys = async () => {
+  const url = exposureNotificationServerUrl;
+  const data = {
+    foo: 'bar',
+  };
 
-const deleteDiagnosisKeyFile = () => {};
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: defaultHeaders,
+      body: JSON.stringify(data),
+    });
 
-const getExposureConfiguration = () => {};
+    const json = await response.json();
+    if (response.ok) {
+      const { bar } = json;
+      return { kind: 'success', body: { bar } };
+    } else {
+      switch (json.error) {
+        default:
+          return { kind: 'failure', error: 'unknown' };
+      }
+    }
+  } catch (e) {
+    return { kind: 'failure', error: 'unknown' };
+  }
+};
 
-const verifyUniqueTestIdentifier = () => {};
+export const getDiagnosisKeyFileURLs = async () => {
+  const url = exposureNotificationServerUrl;
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: defaultHeaders,
+  });
+
+  return wrapResponseBody(response);
+};
+
+export const downloadDiagnosisKeyFile = async () => {};
+
+export const deleteDiagnosisKeyFile = async () => {};
+
+export const getExposureConfiguration = async () => {};
+
+export const verifyUniqueTestIdentifier = async () => {};
+
+const wrapResponseBody = async response => {
+  try {
+    const json = await response.json();
+    if (response.ok) {
+      return { kind: 'success', body: json };
+    } else {
+      return { kind: 'failure', error: json };
+    }
+  } catch (e) {
+    return { kind: 'failure', error: e };
+  }
+};
